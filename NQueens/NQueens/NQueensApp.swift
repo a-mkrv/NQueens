fbf4794e17318903b85322d0208b5458bb556ecd
Anton Makarov <a.mkrv.ios@gmail.com>
2026-02-28 14:50:54 +0100

    Initial commit

---
+++ NQueens/NQueens/NQueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensApp.swift
+//  NQueens
+//
+//  Created by Anton Makarov on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NQueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("NQueensApp")
+        }
+    }
+}
